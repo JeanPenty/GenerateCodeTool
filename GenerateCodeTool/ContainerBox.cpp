2e218842179bd45cb52251bdb9aebcbcac813a46
JeanPenty <ceffans1024@gmail.com>
2025-02-10 15:35:52 +0800

    提交代码

---
+++ GenerateCodeTool/ContainerBox.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+#include "ContainerBox.h"
+
+ContainerBox::ContainerBox(void)
+{
+	
+}
+
+ContainerBox::~ContainerBox(void)
+{
+
+}
+
+void ContainerBox::SetMM2Pix(SStringW& sstrMM2Pix)
+{
+	m_sstrMM2Pix = sstrMM2Pix;
+}
+
+void ContainerBox::SetXDPI(SStringW& sstrXDPI)
+{
+	m_sstrXDPI = sstrXDPI;
+}
+
+void ContainerBox::SetYDPI(SStringW& sstrYDPI)
+{
+	m_sstrYDPI = sstrYDPI;
+}
+
+void ContainerBox::SetCodeType(SStringW& sstrCodeType)
+{
+	m_sstrCodeType = sstrCodeType;
+}
+
+void ContainerBox::SetContent(SStringW& sstrContnet)
+{
+	m_sstrContent = sstrContnet;
+}
+
+void ContainerBox::OnPaint(IRenderTarget* pRT)
+{
+	CRect rcClient = GetClientRect();
+	CPoint ptCenter = rcClient.CenterPoint();
+
+	SStringW sstrFontFormat = L"face:ËÎÌו,bold:0,italic:0,underline:0,strike:0,size:20";
+	SOUI::IFontPtr pFont = SFontPool::GetFont(sstrFontFormat, GetScale());
+	pRT->SelectObject(pFont);
+
+	CRect rcCode;
+	rcCode.left = ptCenter.x - 100;
+	rcCode.right = ptCenter.x + 100;
+	rcCode.top = ptCenter.y - 100;
+	rcCode.bottom = ptCenter.y + 100;
+
+	if (m_sstrCodeType == L"BARCODE_CODE128")
+	{
+// 		struct zint_symbol* symbol;
+// 		symbol = ZBarcode_Create();
+// 		symbol->symbology = BARCODE_CODE128;
+
+		COLORREF clrBkgnd = GETCOLOR(L"RGB(0,255,0)");
+		CAutoRefPtr<IBrush> brush, oldbrush;
+		pRT->CreateSolidColorBrush(clrBkgnd, &brush);
+		pRT->SelectObject(brush, (IRenderObj**)&oldbrush);
+		pRT->FillRectangle(&rcCode);
+		pRT->SelectObject(oldbrush, NULL);
+	}
+}
---
+++ GenerateCodeTool/ContainerBox.h
@@ -0,0 +1,34 @@
+#pragma once
+class ContainerBox : public SWindow
+{
+	DEF_SOBJECT(SWindow, L"containerbox")
+public:
+	ContainerBox(void);
+	~ContainerBox(void);
+
+protected:
+	void OnPaint(IRenderTarget* pRT);
+
+	SOUI_MSG_MAP_BEGIN()
+		MSG_WM_PAINT_EX(OnPaint)
+		SOUI_MSG_MAP_END()
+
+		SOUI_ATTRS_BEGIN()
+		//
+		SOUI_ATTRS_END()
+
+private:
+	SStringW	m_sstrMM2Pix;
+	SStringW	m_sstrXDPI;
+	SStringW	m_sstrYDPI;
+	SStringW	m_sstrCodeType;
+	SStringW	m_sstrContent;
+
+public:
+	void SetMM2Pix(SStringW& sstrMM2Pix);
+	void SetXDPI(SStringW& sstrXDPI);
+	void SetYDPI(SStringW& sstrYDPI);
+	void SetCodeType(SStringW& sstrCodeType);
+	void SetContent(SStringW& sstrContnet);
+};
+
